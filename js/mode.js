ba5770ef9894279dac5af48baf11156d36095bf6
amanbayevv <diyoramanbayev22@gmail.com>
2023-01-19 22:49:10 +0300

    change

--- js/mode.js
+++ js/mode.js
@@ -5,7 +5,9 @@ const lightBtn = document.getElementById('light-btn')
 const modeLocal = localStorage.getItem('mode')
 
 if (modeLocal) {
-    body.classList.add('dark-mode')
+    darkBtn.classList.toggle('hidden')
+    lightBtn.classList.toggle('hidden')
+    body.classList.toggle('dark-mode')
 }
 
 const toggleBtn = () => {
